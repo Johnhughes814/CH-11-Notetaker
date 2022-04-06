9e7e16c8821ea9713925aa4798c60a0b978d4899
Johnhughes814 <8hughes.john@gmail.com>
2022-04-06 18:16:50 -0500

    finished up

--- server.js
+++ server.js
@@ -67,7 +67,35 @@ app.route("/api/notes")
         res.json(newNote);
     });
 
-
+// We need id (not location in array to splice)
+    // This route is dependent on ID of note.
+    //      1. Find note by id via a loop
+    //      2. Splice note out of array of notes.
+    //      3. Re-write db.json, just without that newly deleted note.
+    //=================================================================
+ 
+        app.delete("/api/notes/:id", function (req, res) {
+            let jsonFilePath = path.join(__dirname, "/db/db.json");
+            // request to delete note by id.
+            for (let i = 0; i < database.length; i++) {
+        
+                if (database[i].id == req.params.id) {
+                    // Splice takes i position, and then deletes the 1 note.
+                    database.splice(i, 1);
+                    break;
+                }
+            }
+            // Write the db.json file again.
+            fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
+        
+                if (err) {
+                    return console.log(err);
+                } else {
+                    console.log("Your note was deleted!");
+                }
+            });
+            res.json(database);
+        });
 
 
 
